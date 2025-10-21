77108c04ba80da2212bb83b1ad1add2ffbed02fa
Efe Can KIRBIYIK <kirbiyike22@itu.edu.tr>
2025-10-21 17:17:57 +0300

    fix test parameters

--- tools/test_dist.sh
+++ tools/test_dist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH=./
-GPUS=8
-workdir=./spv2/svg/svg_pointT/spv2-rep
+GPUS=1
+workdir=.
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/test.py \
-	 $workdir/svg_pointT.yaml  $workdir/best.pth --dist
+	 $workdir/configs/svg/svg_pointT.yaml  $workdir/configs/svg/best.pth --dist
